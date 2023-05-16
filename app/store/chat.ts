7fe376667f2c2c9ca47314166838655a566af8df
fengming <zhou_fm@163.com>
2023-05-16 01:23:47 +0000

    add prompt

--- app/store/chat.ts
+++ app/store/chat.ts
@@ -1,7 +1,6 @@
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
-import { type ChatCompletionResponseMessage } from "openai";
 import { trimTopic } from "../utils";
 
 import Locale from "../locales";
