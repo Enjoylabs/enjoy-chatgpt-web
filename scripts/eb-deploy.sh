7456570d8997f21631434275fe30ce3484e58b39
fengming <>
2024-04-20 11:49:47 +0800

    fixed

--- scripts/eb-deploy.sh
+++ scripts/eb-deploy.sh
@@ -1,16 +1,10 @@
 dir="$(dirname "$0")"
-# npm install
-# npm run build
+npm install
+npm run build
+rm -rf .next/cache
 #打包
 rm app_prod.zip
-rm -rf tmp
-mkdir tmp
-cp -R .next/standalone/. tmp/
-cp -R .ebextensions/. tmp/
-cp -R .elasticbeanstalk/. tmp/
-cd tmp
-zip -r app_prod.zip .
+zip -r app_prod.zip package.json .next/ public/ .ebextensions/ .elasticbeanstalk/
 eb deploy --debug
-
 #设置环境变量
 #cat .env.local| xargs eb setenv
